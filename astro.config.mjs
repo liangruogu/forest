b7c9b405ada307a82ce51e95da4baaf13547350c
liangchurong <851903112@qq.com>
2025-04-18 19:58:46 +0800

    pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://liangruogu.github.io",
+  base: "/forest",
   trailingSlash: "always",
   integrations: [
     tailwind(
